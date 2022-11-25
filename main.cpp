0036a2dba3f225b429d54266f2e48628769dd1ff
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-11-25 09:51:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+  
+   int a=0, b=1, c, n;
+    
+    cout << "Inserisci un numero (verranno stampati tutti i numeri della serie minori di quello inserito): ";
+    cin >> n;
+
+        for (int i=0; i<n; i++){
+            c=a;
+            a=b;
+            b=a+c;
+            if (a<=n){
+                cout << a << endl;
+        }else{
+                return 0;
+            }
+        }
 }
