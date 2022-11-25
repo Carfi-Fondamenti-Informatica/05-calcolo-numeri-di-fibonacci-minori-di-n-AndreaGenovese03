0f1b7146fdcbd52b12add02c05e4fbe673ef14bb
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-11-25 09:52:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main() {
   
    int a=0, b=1, c, n;
     
-    cout << "Inserisci un numero (verranno stampati tutti i numeri della serie minori di quello inserito): ";
     cin >> n;
 
         for (int i=0; i<n; i++){
